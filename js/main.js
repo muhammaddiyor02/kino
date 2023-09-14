967771aa1555012b95b694a6f34a56db872c57f6
muhammaddiyor02 <orifonov02@gmail.com>
2023-09-14 14:54:59 +0500

    animation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 var elList = document.querySelector('.list')
 var elSelYear = document.querySelector('.year')
 var elSelReyting = document.querySelector('.reyting')
+var elCatagory = document.querySelector('.catagory')
+var elInp = document.querySelector('.inp')
+
 
 
 var kino = movies.slice(0,200)
+var arrCatagory = []
+kino.forEach((item)=>{
+ if(arrCatagory.includes(item.Categories) == false){
+  arrCatagory.push(item.Categories)
+ }
+})
+arrCatagory.forEach(((item)=>{
+var newOption = document.createElement('option') 
+newOption.textContent = item
+newOption.value = item
+elCatagory.appendChild(newOption)
+
+}))
 function addIt(data){
   elList.innerHTML = ''
   data.forEach((item)=>{
@@ -12,6 +28,8 @@ newLi.innerHTML = `  <div class="card" style="width: 18rem;">
 <img src="https://i.ytimg.com/vi/${item.ytid}/hq720.jpg?" class="card-img-top" alt="">
 <div class="card-body">
   <h5 class="card-title">${item.Title}</h5>
+  <h5 class="card-title">${item.Categories}</h5>
+
   <p class="card-text">${item.imdb_rating}</p>
   <p class="card-text">${item.movie_year}</p>
 
@@ -27,7 +45,7 @@ elList.appendChild(newLi)
 
 elSelYear.addEventListener('change',()=>{
 
-  elList.innerHTML = ''
+
 
  if(elSelYear.value == 'old'){
   addIt( kino.sort((a,b)=> a.movie_year - b.movie_year));
@@ -51,3 +69,18 @@ elSelReyting.addEventListener('change',()=>{
  }  
 
 })
+elCatagory.addEventListener('change', ()=>{
+if(elCatagory.value == ''){
+addIt(kino)
+}else{
+  addIt(kino.filter((item)=>item.Categories == elCatagory.value));
+} 
+})
+
+function fnSearch(){
+  let newKino = kino.filter((item)=>item.Title.toString().toLowerCase().includes(elInp.value.trim().toLowerCase()))
+  newKino.forEach((item)=>{
+      item.Title = item.Title.toLowerCase().replace(elInp.value.toLowerCase(), `<mark>${elInp.value}</mark>`)
+  })
+  addIt(kino.filter((item)=>item.Title.toString().toLowerCase().includes(elInp.value.trim().toLowerCase()) == true))
+}
